68b7ec31af234de50f4662d8bfc1915b69823ebd
Daniel Šípek <dan@sipek.org>
2026-07-12 06:04:47 +0200

    5 - Implemented user config file
    Config file is present
    if not -> user is being prompted to write a correct path
    File is then loaded
    Parsed from JSON to a inner class
    contents are presented to the user in CLI

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿Console.WriteLine("Hello, World!");
-Console.ReadLine();
+﻿using System.Text.Json;
+using ChatAgregator2.data;
+
+var CONFIG_PATH = "config.json";
+
+if (!File.Exists(CONFIG_PATH))
+{
+    Console.WriteLine("Config file  \"config.json\" not found");
+    Console.WriteLine("Please input the proper file name:");
+    CONFIG_PATH = Console.ReadLine();
+    if (!File.Exists(CONFIG_PATH))
+    {
+        //Todo: OpenFileDialog for easier navigation
+        Console.WriteLine("File does not exist, exiting");
+        return;
+    }
+}
+
+var reader = new StreamReader(CONFIG_PATH);
+var userConfig = JsonSerializer.Deserialize<UserConfig>(reader.BaseStream);
+reader.Close();
+
+
+Console.WriteLine("Youtube : " + userConfig.YoutubeUrl);
+Console.WriteLine("Twitch : " + userConfig.TwitchUrl);
+Console.WriteLine("Kick : " + userConfig.KickUrl);
